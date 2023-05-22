a323e9704f17087e2541336ac7bb6f593b8da580
choihyerln <choi.hyehyehihi@gmail.com>
2023-05-22 16:38:09 +0900

    코드업 99번

--- src/codingTest/Q1099.java
+++ src/codingTest/Q1099.java
@@ -1,25 +1,51 @@
 package codingTest;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Q1099 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[11][11];
 		
-		String[] kor= {"AAA", "BCD", "AAAAA", "ZY"};
-		String[] usa= {"AB", "AA", "TTTT"};
-		String[] incs = {"AB BCD AA AAA TTTT AAAAA", "BCD AAA", "AB AAA TTTT BCD", "AA AAAAA AB", "AAA AB BCD"};
+		for (int i=1; i<=10; i++) {
+			for (int j=1; j<=10; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
 		
-		for (int i=0; i<incs.length; i++) {
-			String[] arr = incs[i].split(" ");
-			System.out.println(Arrays.toString(arr));
+		int x = 2;
+		int y = 2;
+		while (true) {
+			// 오른쪽으로 이동
+			if (arr[x][y] == 0) {
+				arr[x][y] = 9;
+				y++;
+			}
+			// 벽에 부딪히면
+			if (arr[x][y] == 1) {
+				y--;
+				x++;
+			}
+			// 먹이 발견
+			if (arr[x][y] == 2) {
+				arr[x][y] = 9;
+				break;
+			}
+			// 가장자리면 9로 바꾸고 멈춰!
+			else if (arr[x][y+1] == 1 && arr[x+1][y] == 1) {
+				if (arr[x][y] == 0) {
+					arr[x][y] = 9;
+					break;
+				}
+			}
+		}
 			
-			for (String str : arr) {
-				if (str.equals(kor[i]))
-					System.out.println("존재합니다.");
+			for (int i=1; i<=10; i++) {
+				for (int j=1; j<=10; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+				System.out.println();
 			}
-				
-			}
-
 	}
 }
